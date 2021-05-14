d6ff60e7698ed6d78bf2953a7f492554e66b18c8
Harry Gustave <harrygustave@harrys-mbp.lan>
2021-05-13 21:52:45 -0500

    added voter routes

--- routes/apiroutes/index.js
+++ routes/apiroutes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+
+router.use(require('./voterRoutes'));
 router.use(require('./candidateRoutes'));
 router.use(require('./partyRoutes'));
 
